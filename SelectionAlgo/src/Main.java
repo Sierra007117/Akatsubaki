5ed8d3724d47dc11e24e6fd7315f55424acd1768
Mahadi Sajjad Neloy <sierra117@qq.com>
2022-05-06 16:55:49 +0600

    SelectionAlgo

---
+++ SelectionAlgo/src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Random;
+class SelectionSort
+{
+    void sort(int[] arr)
+    {
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++)
+        {int min_idx = i;
+            for (int j = i+1; j < n; j++)
+                if (arr[j] < arr[min_idx])
+                    min_idx = j;
+
+            // Swap the found minimum element with the first
+            // element
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+    void printArray(int[] arr)
+    {
+        for (int j : arr) System.out.print(j + " ");
+        System.out.println();
+    }
+    public static void main(String[] args)
+    {
+        SelectionSort ss = new SelectionSort();
+        System.out.print("Random array: ");
+        Random random = new Random();
+        int[] arr = random.ints(20, 10,99).toArray();
+        ss.printArray(arr);
+        ss.sort(arr);
+        System.out.print("Sorted array: ");
+        ss.printArray(arr);
+    }
+}
